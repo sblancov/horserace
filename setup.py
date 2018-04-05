012851285c86d1fdf53e095009d505e03a854145
Santiago Blanco <sblancov84@gmail.com>
2018-04-06 00:06:07 +0200

    chore: Add setup.py to package horserace

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='horserace',
+    version='0.1.0',
+    description='Horse Race Simulator',
+    author='Santiago Blanco Ventas',
+    license='MIT',
+    project_urls={
+        'Source': 'https://github.com/sblancov/horserace'
+    },
+    packages=find_packages(exclude=['docs']),
+    install_requires=['petname'],
+    entry_points={
+        'console_scripts': ['horserace = horserace.horserace:main']
+    }
+)
