98b3806b97893949eae4353b9dcfe0504ca7f76a
Santiago Blanco <sblancov84@gmail.com>
2018-04-17 01:39:03 +0200

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='horserace',
-    version='0.2.0',
+    version='0.3.0',
     description='Horse Race Simulator',
     author='Santiago Blanco Ventas',
     license='MIT',
